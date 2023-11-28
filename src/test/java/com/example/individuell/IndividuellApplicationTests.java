f9d0214ad9bb44ae6fd53e6a01f3a8fd02b113a7
Erimara <erimarandersson@gmail.com>
2023-11-28 15:47:56 +0100

    added test for removal of file and to add file, also added cleanup method for all test data

--- src/test/java/com/example/individuell/IndividuellApplicationTests.java
+++ src/test/java/com/example/individuell/IndividuellApplicationTests.java
@@ -4,35 +4,22 @@ import com.example.individuell.models.File;
 import com.example.individuell.models.User;
 import com.example.individuell.repositories.FileRepository;
 import com.example.individuell.repositories.UserRepository;
+import com.example.individuell.security.Hash;
 import com.example.individuell.services.FileService;
 import com.example.individuell.services.UserService;
+import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.net.URI;
 import java.util.HashMap;
-
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 /*
 * 1: Test to see user registration
@@ -42,53 +29,79 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
 class IndividuellApplicationTests {
-
-    UserService userService;
     UserRepository userRepository;
-
-    FileService fileService;
     FileRepository fileRepository;
-    MongoTemplate mongoTemplate;
-
     @Autowired
-    public IndividuellApplicationTests(UserService userService, UserRepository userRepository,
-                                       FileService fileService, FileRepository fileRepository,
-                                       MongoTemplate mongoTemplate)
+    public IndividuellApplicationTests(UserRepository userRepository, FileRepository fileRepository)
     {
-        this.userService = userService;
         this.userRepository = userRepository;
-        this.fileService = fileService;
         this.fileRepository = fileRepository;
-        this.mongoTemplate = mongoTemplate;
-    }
-
-
-    @Test
-    void contextLoads() {
 
     }
-
-
 
     @Test
     void userRegistration() {
         //Create user
         User testUser = new User();
+        testUser.setId("test-id-user");
         testUser.setEmail("Hello@world.se");
         testUser.setPassword("hello-word");
         //Save user
-        mongoTemplate.save(testUser);
+
+        userRepository.save(testUser);
 
         //Find user
-        User user = userRepository.findByEmail(testUser.getEmail());
+        User user = userRepository.findById(testUser.getId()).orElseThrow(()-> new RuntimeException("Could not find user"));
 
         //Assert user
-        assertNotNull(user);
-        System.out.println("Saved User: " + testUser);
-        System.out.println("Retrieved User: " + user);
+        Assertions.assertNotNull(user);
+        Assertions.assertTrue(userRepository.existsById(user.getId()));
+
+    }
+
+    @Test
+    void uploadFile() throws Exception {
+        HashMap<String, String> fileMap = new HashMap<>();
+        byte[] fileContent = "Testing file upload".getBytes();
+        MockMultipartFile mockFile = new MockMultipartFile("file", "test.txt", "text/plain", fileContent);
+        fileMap.put("Filename: ", mockFile.getOriginalFilename());
+        fileMap.put("Bytes: ", mockFile.getBytes().toString());
+        fileMap.put("File type: ", mockFile.getContentType());
+        File savedFile = new File();
+        savedFile.setFileProperties(fileMap);
+        savedFile.setId("test-id-file");
+
+        User mockUser = new User();
+        mockUser.setId("test-id-user");
+        mockUser.setEmail("Hello@world.se");
+        mockUser.setPassword("hello-world");
+        savedFile.setFileOwner(mockUser);
+
+
+        fileRepository.save(savedFile);
+        File retrieveSavedFile = fileRepository.findById(savedFile.getId()).orElseThrow(() -> new RuntimeException("File not found"));
+        Assertions.assertTrue(fileRepository.existsById(retrieveSavedFile.getId()));
+    }
+
+    @Test
+    void removeFile(){
+        File testFile = new File();
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("key","value");
+        testFile.setId("test-id-remove-file");
+        testFile.setFileProperties(hashMap);
+        fileRepository.save(testFile);
+        fileRepository.findById("test-id-remove-file").ifPresent(presentFile -> fileRepository.deleteById(presentFile.getId()));
+        Assertions.assertFalse(fileRepository.existsById(testFile.getId()));
+    }
+
+    @AfterEach
+    void removeTestData(){
+        fileRepository.findById("test-id-file").ifPresent(presentFile -> fileRepository.deleteById(presentFile.getId()));
+        userRepository.findById("test-id-user").ifPresent(presentUser -> userRepository.deleteById(presentUser.getId()));
     }
 
 
